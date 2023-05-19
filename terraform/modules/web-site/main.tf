3b3f000144de24b629e8c26c0844ec746f3caf71
Cadiastands <52361678+Cadiastands@users.noreply.github.com>
2023-05-19 12:08:42 -0300

    fixed buckets

--- terraform/modules/web-site/main.tf
+++ terraform/modules/web-site/main.tf
@@ -58,10 +58,38 @@ resource "aws_s3_bucket_ownership_controls" "website" {
   }
 }
 
-resource "aws_s3_bucket_acl" "website_acl" {
+# 
+
+
+
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website,
+    aws_s3_bucket_public_access_block.website,
+  ]
+
   bucket = aws_s3_bucket.website.id
   acl    = "public-read"
 }
+
+# resource "aws_s3_bucket_acl" "website_acl" {
+#   bucket = aws_s3_bucket.website.id
+#   acl    = "public-read"
+# }
+
+
+
+
 
 resource "aws_s3_bucket_policy" "website_policy" {
   bucket = aws_s3_bucket.website.id
