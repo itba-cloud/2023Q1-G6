a63e5c77eab9faeabdff23920de71db8323f8af5
Cadiastands <52361678+Cadiastands@users.noreply.github.com>
2023-05-19 11:49:20 -0300

    proxy disablesd and rds working

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -3,7 +3,8 @@ resource "aws_db_instance" "this" {
   allocated_storage     = local.allocated_storage
   max_allocated_storage = local.max_allocated_storage
   multi_az              = true
-  db_name               = local.db_name
+  # db_name               = local.db_name
+  db_name = "MyDB"
   engine                = local.engine
   engine_version        = local.engine_version
   instance_class        = local.instance_class
@@ -20,7 +21,7 @@ resource "aws_db_instance" "this" {
 }
 
 resource "aws_db_subnet_group" "this" {
-  name       = "db_subnet_group"
+  name       = "db_subnet_group-167281"
   subnet_ids = var.vpc_subnet_ids
 
   tags = {
@@ -28,60 +29,60 @@ resource "aws_db_subnet_group" "this" {
   }
 }
 
-resource "aws_db_proxy" "db_proxy" {
-  name                = "${local.db_name}-proxy"
-  debug_logging       = false
-  engine_family       = local.engine
-  idle_client_timeout = 1800
-  require_tls         = true
-  role_arn            = aws_iam_role.rds_proxy_role.arn
-  # vpc_security_group_ids = [aws_security_group.example.id]
-  vpc_subnet_ids = var.vpc_subnet_ids
+# resource "aws_db_proxy" "db_proxy" {
+#   name                = "${local.db_name}-proxy"
+#   debug_logging       = false
+#   engine_family       = local.engine
+#   idle_client_timeout = 1800
+#   require_tls         = true
+#   role_arn            = aws_iam_role.rds_proxy_role.arn
+#   # vpc_security_group_ids = [aws_security_group.example.id]
+#   vpc_subnet_ids = var.vpc_subnet_ids
 
 
-  auth {
+#   auth {
 
-  }
+#   }
 
-  tags = {
-    name = "${local.db_name}-proxy"
-  }
-}
+#   tags = {
+#     name = "${local.db_name}-proxy"
+#   }
+# }
 
-resource "aws_db_proxy_default_target_group" "db_proxy_default_target_group" {
-  db_proxy_name = aws_db_proxy.db_proxy.name
+# resource "aws_db_proxy_default_target_group" "db_proxy_default_target_group" {
+#   db_proxy_name = aws_db_proxy.db_proxy.name
 
-}
+# }
 
-resource "aws_db_proxy_target" "proxy_target" {
-  db_instance_identifier = aws_db_instance.this.id
-  db_proxy_name          = aws_db_proxy.db_proxy.name
-  target_group_name      = aws_db_proxy_default_target_group.db_proxy_default_target_group.name
-}
+# resource "aws_db_proxy_target" "proxy_target" {
+#   db_instance_identifier = aws_db_instance.this.id
+#   db_proxy_name          = aws_db_proxy.db_proxy.name
+#   target_group_name      = aws_db_proxy_default_target_group.db_proxy_default_target_group.name
+# }
 
-resource "aws_db_proxy_endpoint" "proxy_endpoint" {
-  db_proxy_name          = aws_db_proxy.db_proxy.name
-  db_proxy_endpoint_name = "proxy-endpoint"
-  vpc_subnet_ids         = var.vpc_subnet_ids
-}
+# resource "aws_db_proxy_endpoint" "proxy_endpoint" {
+#   db_proxy_name          = aws_db_proxy.db_proxy.name
+#   db_proxy_endpoint_name = "proxy-endpoint"
+#   vpc_subnet_ids         = var.vpc_subnet_ids
+# }
 
-resource "aws_iam_role" "rds_proxy_role" {
-  name = "RDSProxyRole"
+# resource "aws_iam_role" "rds_proxy_role" {
+#   name = "RDSProxyRole"
 
-  assume_role_policy = data.aws_iam_policy_document.example.json
-}
-data "aws_iam_policy_document" "example" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["rds.amazonaws.com"]
-    }
-  }
-}
+#   assume_role_policy = data.aws_iam_policy_document.example.json
+# }
+# data "aws_iam_policy_document" "example" {
+#   statement {
+#     effect  = "Allow"
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["rds.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role_policy_attachment" "rds_proxy_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSDataFullAccess"
-  role       = aws_iam_role.rds_proxy_role.name
-}
+# resource "aws_iam_role_policy_attachment" "rds_proxy_policy_attachment" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSDataFullAccess"
+#   role       = aws_iam_role.rds_proxy_role.name
+# }
